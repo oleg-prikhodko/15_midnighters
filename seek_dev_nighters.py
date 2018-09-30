6ba27748ad18b0142c90ebb0fd36cbc77bf781f7
Oleg <super.oleg.3000@yandex.ru>
2018-09-30 23:51:11 +0300

    Initial version

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,46 @@
+import json
+import sys
+from datetime import datetime
+from urllib.error import URLError
+from urllib.request import urlopen
+
+from pytz import timezone, utc
+
+
 def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+    page = 1
+    number_of_pages = 1
+    api_url = "http://devman.org/api/challenges/solution_attempts/?page={}"
+
+    while page <= number_of_pages:
+        response = urlopen(api_url.format(page))
+        attempts_info = json.loads(response.read())
+        solution_attempts = attempts_info["records"]
+        number_of_pages = attempts_info["number_of_pages"]
+        page += 1
+
+        yield from solution_attempts
+
 
 def get_midnighters():
-    pass
+    datetime_format = "%Y-%m-%d %H:%M %Z%z"
+    since_hour = 0
+    until_hour = 5
 
-if __name__ == '__main__':
-  pass
+    for solution_attempt in load_attempts():
+        user_timezone = timezone(solution_attempt["timezone"])
+        utc_time = datetime.fromtimestamp(solution_attempt["timestamp"], utc)
+        user_time = utc_time.astimezone(user_timezone)
+
+        if user_time.hour in range(since_hour, until_hour):
+            yield solution_attempt["username"], user_time.strftime(
+                datetime_format
+            )
+
+
+if __name__ == "__main__":
+    try:
+        for midnighter, submission_time in get_midnighters():
+            print(midnighter, submission_time)
+    except URLError as error:
+        sys.exit(error)
