cdf6ee39c786b256dddbe403d0c8f3b271e9fcf0
Oleg <super.oleg.3000@yandex.ru>
2018-10-02 12:30:09 +0300

    Fixes

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,22 +1,20 @@
-import json
 import sys
+from collections import defaultdict
 from datetime import datetime
-from urllib.error import URLError
-from urllib.parse import urlencode
-from urllib.request import urlopen
 
+import requests
 from pytz import timezone, utc
 
 
 def load_attempts():
     page = 1
     number_of_pages = 1
-    api_url = "http://devman.org/api/challenges/solution_attempts/?"
+    api_url = "http://devman.org/api/challenges/solution_attempts/"
 
     while page <= number_of_pages:
         url_params = {"page": page}
-        response = urlopen(api_url + urlencode(url_params))
-        attempts_info = json.loads(response.read())
+        response = requests.get(api_url, params=url_params)
+        attempts_info = response.json()
         solution_attempts = attempts_info["records"]
         number_of_pages = attempts_info["number_of_pages"]
         page += 1
@@ -24,25 +22,33 @@ def load_attempts():
         yield from solution_attempts
 
 
-def get_midnighters():
+def get_midnighters(solution_attempts):
     datetime_format = "%Y-%m-%d %H:%M %Z%z"
     since_hour = 0
     until_hour = 5
 
-    for solution_attempt in load_attempts():
+    midnighters = defaultdict(list)
+
+    for solution_attempt in solution_attempts:
         user_timezone = timezone(solution_attempt["timezone"])
-        utc_time = datetime.fromtimestamp(solution_attempt["timestamp"], utc)
-        user_time = utc_time.astimezone(user_timezone)
+        user_time = datetime.fromtimestamp(
+            solution_attempt["timestamp"], user_timezone
+        )
 
         if user_time.hour in range(since_hour, until_hour):
-            yield solution_attempt["username"], user_time.strftime(
-                datetime_format
+            midnighters[solution_attempt["username"]].append(
+                user_time.strftime(datetime_format)
             )
+
+    return midnighters
 
 
 if __name__ == "__main__":
     try:
-        for midnighter, submission_time in get_midnighters():
-            print(midnighter, submission_time)
-    except URLError as error:
+        midnighters = get_midnighters(load_attempts())
+        for username, submission_times in midnighters.items():
+            print(username)
+            for submission_time in submission_times:
+                print("\t" + submission_time)
+    except requests.RequestException as error:
         sys.exit(error)
